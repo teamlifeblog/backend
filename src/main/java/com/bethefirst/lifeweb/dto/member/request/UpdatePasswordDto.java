c9ba7457ae699f8030a6c51fa03b325c88899e8a
biber <dlehdxo2@gmail.com>
2023-02-26 17:34:46 +0900

    📝: 추가 AllArgsConstructor
    1. 문서작성을 위한 생성자 추가

--- src/main/java/com/bethefirst/lifeweb/dto/member/request/UpdatePasswordDto.java
+++ src/main/java/com/bethefirst/lifeweb/dto/member/request/UpdatePasswordDto.java
@@ -1,12 +1,14 @@
 package com.bethefirst.lifeweb.dto.member.request;
 
 import com.bethefirst.lifeweb.entity.member.Member;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class UpdatePasswordDto {
 
     private String newPassword;
